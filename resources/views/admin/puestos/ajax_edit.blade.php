2e5af7754fef1c59d84dd56bb63395e983bceb87
Amado J. Ramos Arboleda <amado0529@gmail.com>
2015-04-26 12:57:01 -0500

    Error solved

--- resources/views/admin/puestos/ajax_edit.blade.php
+++ resources/views/admin/puestos/ajax_edit.blade.php
@@ -1,12 +1,12 @@
+@include('admin.partials.messages')
+{!! Form::model($user,['route' => ['administrador.puestos.update',$user->id], 'method' => 'PUT']) !!}
 <div class="modal-body">
   <div class="panel-body">
-    @include('admin.partials.messages')
-    {!! Form::model($user,['route' => ['administrador.puestos.update',$user->id], 'method' => 'PUT']) !!}
     @include('admin.puestos.partials.fields')
   </div>                  
 </div>
 <div class="modal-footer">
   <button type="submit" class="btn btn-success">Editar puesto de votacion</button>
-  {!! Form::close() !!} 
   <button data-dismiss="modal" class="btn btn-danger" type="button">Cerrar</button>
 </div>
+{!! Form::close() !!} 
